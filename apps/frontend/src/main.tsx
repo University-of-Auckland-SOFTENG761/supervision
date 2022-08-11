ab87301d4f1d0030186d2df386bde9dfaf4e2164
Jackson Chadfield <jcha686@aucklanduni.ac.nz>
2022-08-11 13:16:45 +1200

    add a basic service worker to cache files for offline use

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -4,9 +4,16 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './app/app';
 
+if ("serviceWorker" in navigator){
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js");
+  })
+}
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <StrictMode>
     <BrowserRouter>
---
+++ apps/frontend/webpack.config.js
@@ -0,0 +1,14 @@
+const { merge } = require('webpack-merge');
+const { GenerateSW } = require("workbox-webpack-plugin");
+
+
+module.exports = (config, context) => {
+
+  return merge(config, {
+    plugins: [
+      new GenerateSW({
+        swDest: 'sw.js',
+      })
+    ],
+  });
+};
