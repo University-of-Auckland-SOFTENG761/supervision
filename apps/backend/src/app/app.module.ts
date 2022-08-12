b7b5df846e43d44e36ece60cb119c34243e2413c
Jackson Chadfield <jcha686@aucklanduni.ac.nz>
2022-08-13 10:57:53 +1200

    add graphql support

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import configuration, {
   configSchema,
   TypeOrmConfigService,
@@ -16,6 +18,10 @@ import { UsersModule } from '@supervision/users/users.module';
     }),
     TypeOrmModule.forRootAsync({
       useClass: TypeOrmConfigService,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
     }),
     UsersModule,
   ],
