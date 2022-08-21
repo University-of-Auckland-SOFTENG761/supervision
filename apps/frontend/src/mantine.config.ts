c1f81fdf0769a4b6b1a9df88b9d7a75957d1d3aa
uoa-ypec413 <ypec413@aucklanduni.ac.nz>
2022-08-21 21:01:34 +1200

    Add patient tabs

--- apps/frontend/src/mantine.config.ts
+++ apps/frontend/src/mantine.config.ts
@@ -1,7 +1,7 @@
 import { MantineThemeOverride } from '@mantine/core';
 
 const theme: MantineThemeOverride = {
-  primaryColor: 'red',
+  primaryColor: 'blue',
   primaryShade: 5,
   colors: {
     red: [
