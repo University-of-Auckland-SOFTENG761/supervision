d4f650287aa866b44bfb714f05c4ae6f4936f12c
Yulia Pechorina <68877945+uoa-ypec413@users.noreply.github.com>
2022-08-23 12:26:54 +1200

    Update apps/frontend/src/utils/date.utils.ts
    Co-authored-by: Keith Anderson <68878136+kand198@users.noreply.github.com>

--- apps/frontend/src/utils/date.utils.ts
+++ apps/frontend/src/utils/date.utils.ts
@@ -3,7 +3,6 @@ export const calculateAge = (birthdayDate?: Date) => {
   if (!birthdayDate) {
     return undefined;
   }
-  console.log(birthdayDate);
   const ageDifMs = Date.now() - birthdayDate.getTime();
   const ageDate = new Date(ageDifMs);
   return Math.abs(ageDate.getUTCFullYear() - 1970);
