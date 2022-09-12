43185a88e1d87ea6c5cb0ef8e3f3f45fee643d7f
uoa-ypec413 <ypec413@aucklanduni.ac.nz>
2022-09-12 16:40:51 +1200

    Check for internet connection

--- apps/frontend/src/app/shared/hooks/use-network.ts
+++ apps/frontend/src/app/shared/hooks/use-network.ts
@@ -1,14 +1,39 @@
 import { useEffect, useState } from 'react';
 
+/*
+Pings google.com to check for internet connection.
+ */
+const ping = async () => {
+  if (!navigator.onLine) return false;
+
+  try {
+    await fetch('https://www.google.com/', {
+      mode: 'no-cors',
+    });
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
 export const useNetwork = () => {
   const [online, setOnline] = useState(navigator.onLine);
+  console.log(online);
 
-  const goOnline = () => setOnline(true);
+  const checkOnline = async () => {
+    // Ping google.com to check for internet connection (navigator.onLine only checks network connection)
+    const online = await ping();
+    setOnline(online);
+  };
+
   const goOffline = () => setOnline(false);
+  const goOnline = () => setOnline(true);
 
   useEffect(() => {
     window.addEventListener('online', goOnline);
     window.addEventListener('offline', goOffline);
+
+    checkOnline();
     return () => {
       window.removeEventListener('online', goOnline);
       window.removeEventListener('offline', goOffline);
