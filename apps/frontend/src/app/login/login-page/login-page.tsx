791336e86bf2c28df91e075b87644dde5751b9f4
uoa-ypec413 <ypec413@aucklanduni.ac.nz>
2022-09-13 15:29:59 +1200

    Remove location select from login page

--- apps/frontend/src/app/login/login-page/login-page.tsx
+++ apps/frontend/src/app/login/login-page/login-page.tsx
@@ -1,29 +1,16 @@
 import React from 'react';
-import { Select, Stack, Text, Title } from '@mantine/core';
+import { Stack, Text, Title } from '@mantine/core';
 import { Button, Logo } from '@shared';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useNetwork } from '@shared';
 import { OfflineLoginPage } from '../offline-login-page';
 
-// TODO: Replace with actual data from database
-const locations = ['University of Auckland', 'Harvard University', 'MIT'];
-
 export const LoginPage = () => {
   const { loginWithRedirect } = useAuth0();
   const { online } = useNetwork();
 
-  const [location, setLocation] = React.useState<string | null>(null);
-  const [selectErrorMessage, setSelectErrorMessage] = React.useState<
-    string | undefined
-  >(undefined);
-
   const handleClickLogin = () => {
-    if (location === null) {
-      setSelectErrorMessage('Please select a location');
-    } else {
-      setSelectErrorMessage(undefined);
-      loginWithRedirect();
-    }
+    loginWithRedirect();
   };
 
   if (!online) {
@@ -36,17 +23,6 @@ export const LoginPage = () => {
           Welcome to SuperVision!
         </Title>
         <Text className="w-full -mt-4">Please log in</Text>
-        <Select
-          label="Select a location:"
-          placeholder="Pick one"
-          data={locations}
-          value={location}
-          onChange={(value) => setLocation(value)}
-          className="w-full"
-          searchable
-          required
-          error={selectErrorMessage}
-        />
         <Button uppercase fullWidth size="lg" onClick={handleClickLogin}>
           Login with Auth0
         </Button>
