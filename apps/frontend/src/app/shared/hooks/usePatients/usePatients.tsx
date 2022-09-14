b18865e62b5112e054f11bd7c03f404e1697cc7c
Keith Anderson <kand198@aucklanduni.ac.nz>
2022-09-14 16:08:04 +1200

    match project file structure

---
+++ apps/frontend/src/app/shared/hooks/usePatients/usePatients.tsx
@@ -0,0 +1,48 @@
+import { IPatient } from '@patients';
+import { uuid } from 'uuidv4';
+import { useState, useEffect, useCallback } from 'react';
+import { RxDatabase } from 'rxdb';
+import { patientDatabase } from 'database';
+
+export const usePatients = () => {
+  const [patientsDb, setPatientsDb] = useState<RxDatabase>();
+  const [patients, setPatients] = useState<IPatient[]>([]);
+
+  const newPatient = useCallback(() => {
+    const newPatient = {
+      id: uuid(),
+    };
+    patientsDb?.['patients'].insert(newPatient);
+    return newPatient.id;
+  }, [patientsDb]);
+
+  const updatePatient = (patient: IPatient) => {
+    const oldPatient = patients.find((p) => p.id === patient.id);
+    if (oldPatient) {
+      // Check for deep equality
+      if (JSON.stringify(oldPatient) === JSON.stringify(patient)) {
+        return;
+      }
+    }
+    patientsDb?.['patients'].atomicUpsert(patient);
+  };
+
+  useEffect(() => {
+    if (!patientsDb) {
+      patientDatabase
+        .get()
+        ?.then((db: RxDatabase | null) => {
+          db && setPatientsDb(db);
+          db && db['patients'].find().$.subscribe(setPatients);
+          return () => db?.['patients'].find().$.unsubscribe();
+        })
+        .catch((error: Error) => {
+          console.error(error);
+        });
+    }
+  }, [patientsDb]);
+
+  return { patients, newPatient, updatePatient };
+};
+
+export default usePatients;
