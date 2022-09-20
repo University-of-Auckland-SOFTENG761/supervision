f519b7a37f0e27d16a21f84eba596d6930acde27
Harris <htom380@aucklanduni.ac.nz>
2022-09-21 09:47:29 +1200

    Fixes botched merge for user.module

--- apps/backend/src/app/users/users.module.ts
+++ apps/backend/src/app/users/users.module.ts
@@ -4,13 +4,13 @@ import { AuthModule } from '@supervision/auth';
 import { UserEntity } from '@supervision/users/database';
 import { UserResolver } from '@supervision/users/graphql';
 import { UserService } from '@supervision/users/users.service';
+import { PatientEntity } from '@supervision/patients';
+import { ConsultEntity } from '@supervision/consults';
 
 @Module({
   imports: [
-
     TypeOrmModule.forFeature([UserEntity]),
     forwardRef(() => AuthModule),
-  ,
     TypeOrmModule.forFeature([PatientEntity]),
     TypeOrmModule.forFeature([ConsultEntity]),
   ],
