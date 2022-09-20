d9bc21c83a6bd5cd370c983f5db8118566abefa3
Jackson Chadfield <jcha686@aucklanduni.ac.nz>
2022-09-20 15:26:11 +1200

    fix authorisation not using environment

--- apps/frontend/src/database/authorisation.ts
+++ apps/frontend/src/database/authorisation.ts
@@ -1,13 +1,13 @@
 import { Auth0Client } from '@auth0/auth0-spa-js';
-import { AUTH0_CLIENT_ID, AUTH0_DOMAIN, AUTH0_REDIRECT_URI } from '@login';
+import environment from '@environment';
 
 let idToken: string | undefined = undefined;
 
 export const getIdToken = async () => {
   const client = new Auth0Client({
-    domain: AUTH0_DOMAIN ?? '',
-    client_id: AUTH0_CLIENT_ID ?? '',
-    redirect_uri: AUTH0_REDIRECT_URI ?? '',
+    domain: environment.auth0.domain,
+    client_id: environment.auth0.client_id,
+    redirect_uri: environment.auth0.redirect_uri,
     scope: 'openid',
     responseType: 'code',
     cacheLocation: 'localstorage',
