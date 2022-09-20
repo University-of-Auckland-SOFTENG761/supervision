34af9a92762d2944ec576b8bf1990f6258cdbe76
Keith Anderson <68878136+kand198@users.noreply.github.com>
2022-09-20 15:38:44 +1200

    feat(frontend, auth): connect auth to RxDB (#13)
    * Link RxDB to FE authentication
    
    Fix some bugs
    
    more fixes
    
    Small improvements
    
    wip
    
    why does this kinda work :(
    
    Refactor usePatients hook into PatientsContext
    
    Co-authored-by: Yulia Pechorina <uoa-ypec413@users.noreply.github.com>
    
    Fix things
    
    Remove console.logs
    
    * Fix error parsing
    
    * Add offline initials to avatar, fix warning
    
    * Try fix incorrect modal behaviour
    
    * Add logic to deal with users with different types
    
    * Only display unhandled errors
    
    * Don't spam the backend if unauthenticated
    
    * Remove unnecessary log statements
    
    * use environment file for configuration
    
    * fix authorisation not using environment
    
    Co-authored-by: Jackson Chadfield <jcha686@aucklanduni.ac.nz>

--- apps/frontend/src/environments/environment.ts
+++ apps/frontend/src/environments/environment.ts
@@ -1,6 +1,16 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // When building for production, this file is replaced with `environment.prod.ts`.
 
-export const environment = {
+import { IEnvironment } from './environment.interface';
+
+const environment: IEnvironment = {
   production: false,
+  api_url: new URL('http://localhost:3333'),
+  auth0: {
+    domain: 'dev-6mx9ad29.us.auth0.com',
+    client_id: 'SiqwTzesySraxm8Ml8t7wModRdckoh0c',
+    redirect_uri: 'http://localhost:4200/patient-details',
+  },
 };
+
+export default environment;
