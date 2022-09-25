b61655f7b65552b58df181ae06c4df5a4e4bceb7
hmit106 <hmit106@aucklanduni.ac.nz>
2022-09-26 10:50:59 +1300

    Fix failing test due to lack of Router in test render

--- apps/frontend/src/app/consults/consult-details-page/consult-details-page.spec.tsx
+++ apps/frontend/src/app/consults/consult-details-page/consult-details-page.spec.tsx
@@ -1,9 +1,14 @@
 import { render } from '@testing-library/react';
 import ConsultDetails from './consult-details-page';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('Consult Details Page', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<ConsultDetails />);
+    const { baseElement } = render(
+      <BrowserRouter>
+        <ConsultDetails />
+      </BrowserRouter>
+    );
     expect(baseElement).toBeTruthy();
   });
 });
