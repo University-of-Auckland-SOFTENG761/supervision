9e99fbdae49f974e1b58ca78ec3e8f9d4db1b473
Keith Anderson <kand198@aucklanduni.ac.nz>
2022-10-02 21:23:22 +1300

    fix fe tests

--- apps/frontend/src/app/consults/consult-details-page/consult-details-page.spec.tsx
+++ apps/frontend/src/app/consults/consult-details-page/consult-details-page.spec.tsx
@@ -1,9 +1,14 @@
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import ConsultDetails from './consult-details-page';
 
 describe('Consult Details Page', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<ConsultDetails />);
+    const { baseElement } = render(
+      <MemoryRouter>
+        <ConsultDetails />
+      </MemoryRouter>
+    );
     expect(baseElement).toBeTruthy();
   });
 });
