93764f3466c6c319c3c46a4752b021f364497569
uoa-ypec413 <ypec413@aucklanduni.ac.nz>
2022-10-03 16:45:22 +1300

    Fix circular dependency

--- apps/backend/src/app/consults/consults.module.ts
+++ apps/backend/src/app/consults/consults.module.ts
@@ -3,7 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConsultEntity } from '@supervision/consults/database/consult.entity';
 import { ConsultsResolver } from '@supervision/consults/graphql/consults.resolver';
 import { ConsultsService } from '@supervision/consults/consults.service';
-import { UsersModule } from '@supervision/users/users.module';
+import { UsersModule } from '@supervision/users';
 
 @Module({
   imports: [
