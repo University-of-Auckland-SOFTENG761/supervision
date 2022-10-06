b7a34be6b2889452a7abc23f0ad659d3930f287a
uoa-lzha989 <68832471+uoa-lzha989@users.noreply.github.com>
2022-10-07 00:12:45 +1300

    router for test

--- apps/frontend/src/app/login/login-page/login-page.spec.tsx
+++ apps/frontend/src/app/login/login-page/login-page.spec.tsx
@@ -1,9 +1,14 @@
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import LoginPage from './login-page';
 
 describe('Login Page', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<LoginPage />);
+    const { baseElement } = render(
+      <MemoryRouter>
+        <LoginPage />
+      </MemoryRouter>
+    );
     expect(baseElement).toBeTruthy();
   });
 });
