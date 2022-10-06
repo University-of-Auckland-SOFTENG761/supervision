4c500902bea8a0adf5a930c4b31493b9e037157b
Joel <joelkahuhutch@gmail.com>
2022-10-06 20:02:04 +1300

    fix: remove that pesky linter warning

--- apps/frontend/src/app/spectacles/spectacles-list-page/spectacles-list-page.tsx
+++ apps/frontend/src/app/spectacles/spectacles-list-page/spectacles-list-page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { ISpectacles } from '../spectacles-details-page';
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
