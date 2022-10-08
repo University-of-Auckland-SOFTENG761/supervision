9358a98ad6803f098f98da8e4efbaa87c3d255e6
hmit106 <hmit106@aucklanduni.ac.nz>
2022-10-09 01:49:26 +1300

    Update recalls list to work with performance PR

--- apps/frontend/src/app/consults/recalls/recalls-list-page/recalls-list-page.tsx
+++ apps/frontend/src/app/consults/recalls/recalls-list-page/recalls-list-page.tsx
@@ -8,60 +8,224 @@ import { useDebouncedValue } from '@mantine/hooks';
 import sortBy from 'lodash/sortBy';
 import { DataTable, DataTableSortStatus } from 'mantine-datatable';
 import { useDatabase } from '@shared';
+import { ConsultDocType, PatientDocType } from '../../../../database';
+import { RxDocument } from 'rxdb';
+import { OrderStatus } from '../../../spectacles/spectacles-details-page';
+import styles from '../../../spectacles/spectacles-list-page/spectacles-list.module.scss';
+import { DatePicker } from '@mantine/dates';
+
+type RecordType = Partial<ConsultDocType> & Partial<PatientDocType>;
 
 export const RecallsListPage = () => {
-  const { consults } = useDatabase();
-
+  const { consultsCollection, patientsCollection } = useDatabase();
   const navigate = useNavigate();
 
-  const [isFetching] = useState(false);
+  const [matchingConsults, setMatchingConsults] = useState<
+    RxDocument<ConsultDocType>[]
+  >([]);
+  const [matchingPatients, setMatchingPatients] = useState<
+    RxDocument<PatientDocType>[]
+  >([]);
+  const [recallRecords, setRecallRecords] = useState<RecordType[]>();
+  const [tableRecords, setTableRecords] = useState(
+    sortBy(recallRecords, 'name')
+  );
   const [sortStatus, setSortStatus] = useState<DataTableSortStatus>({
-    columnAccessor: 'date',
+    columnAccessor: 'recallDate',
     direction: 'asc',
   });
-  const [tableRecords, setTableRecords] = useState(
-    sortBy(consults, 'recallDate')
-  );
-  useEffect(() => {
-    const data = sortBy(consults, sortStatus.columnAccessor);
-    setTableRecords(sortStatus.direction === 'desc' ? data.reverse() : data);
-  }, [sortStatus]);
+
   // Search bar text query to filter results by
   const [query, setQuery] = useState('');
   const [debouncedQuery] = useDebouncedValue(query, 200);
   // Select order status query to filter results by
-  const [statusQuery, setStatusQuery] = useState<string | null>(null);
-  const [debouncedStatusQuery] = useDebouncedValue(statusQuery, 200);
+  const [dateQuery, setDateQuery] = useState<Date | null>(null);
+  const [debouncedDateQuery] = useDebouncedValue(dateQuery, 200);
 
   useEffect(() => {
-    setTableRecords(
-      (consults ?? []).filter(({ recallDate, recallDescription }) => {
-        if (debouncedStatusQuery !== '' && debouncedStatusQuery !== null) {
-          return false;
-        }
-        if (
-          debouncedQuery !== '' &&
-          !`$${recallDate} ${recallDescription}`
-            .toLowerCase()
-            .includes(debouncedQuery.trim().toLowerCase())
-        ) {
-          return false;
-        }
-        return true;
-      })
-    );
-  }, [debouncedQuery, debouncedStatusQuery]);
+    const data = sortBy(recallRecords, sortStatus.columnAccessor);
+    setTableRecords(sortStatus.direction === 'desc' ? data.reverse() : data);
+  }, [sortStatus]);
+
+  // Find all patients and consults that could match the query
+  useEffect(() => {
+    if (patientsCollection && consultsCollection) {
+      patientsCollection
+        .find(
+          debouncedQuery
+            ? {
+                selector: {
+                  $or: [
+                    {
+                      firstName: {
+                        $regex: new RegExp(
+                          debouncedQuery
+                            .trim()
+                            .replace(/\s+/g, ' ')
+                            .split('')
+                            .join('.*'),
+                          'i'
+                        ),
+                        $options: 'i',
+                      },
+                    },
+                    {
+                      firstName: {
+                        $regex: new RegExp(
+                          debouncedQuery
+                            .slice(debouncedQuery.indexOf(' ') + 1)
+                            .trim()
+                            .replace(/\s+/g, ' ')
+                            .split('')
+                            .join('.*'),
+                          'i'
+                        ),
+                        $options: 'i',
+                      },
+                    },
+                    {
+                      lastName: {
+                        $regex: new RegExp(
+                          debouncedQuery
+                            .trim()
+                            .replace(/\s+/g, ' ')
+                            .split('')
+                            .join('.*'),
+                          'i'
+                        ),
+                        $options: 'i',
+                      },
+                    },
+                    {
+                      lastName: {
+                        $regex: new RegExp(
+                          debouncedQuery
+                            .slice(debouncedQuery.indexOf(' ') + 1)
+                            .trim()
+                            .replace(/\s+/g, ' ')
+                            .split('')
+                            .join('.*'),
+                          'i'
+                        ),
+                        $options: 'i',
+                      },
+                    },
+                  ],
+                },
+                limit: 100,
+              }
+            : undefined
+        )
+        .exec()
+        .then((patients) => {
+          setMatchingPatients((p) => [...p, ...patients]);
+          patients.forEach((patient) => {
+            consultsCollection
+              .findByIds(patient.consultIds ?? [])
+              .then((newConsults) => {
+                const newConsultsArray = Array.from(newConsults.values());
+                newConsults &&
+                  setMatchingConsults((c) => [...c, ...newConsultsArray]);
+              });
+          });
+        });
+
+      consultsCollection
+        .find(
+          debouncedQuery
+            ? {
+                selector: {
+                  $or: [
+                    {
+                      recallDescription: {
+                        $regex: new RegExp(
+                          debouncedQuery
+                            .trim()
+                            .replace(/\s+/g, ' ')
+                            .split('')
+                            .join('.*'),
+                          'i'
+                        ),
+                        $options: 'i',
+                      },
+                    },
+                  ],
+                },
+                limit: 100,
+              }
+            : undefined
+        )
+        .exec()
+        .then((consults) => {
+          setMatchingConsults((c) => [...c, ...consults]);
+          consults.forEach((consult) => {
+            patientsCollection
+              .findOne(consult.patientId)
+              .exec()
+              .then((patient) => {
+                patient && setMatchingPatients((prev) => [...prev, patient]);
+              });
+          });
+        });
+    }
+  }, [debouncedQuery, patientsCollection, consultsCollection]);
+
+  useEffect(() => {
+    setMatchingPatients([]);
+    setMatchingConsults([]);
+  }, [debouncedQuery]);
+
+  // Combine the matching patients and consults into a single array of records
+  useEffect(() => {
+    if (matchingPatients && matchingConsults) {
+      const records = matchingPatients
+        .reduce((a: RecordType[], p) => {
+          const consults = matchingConsults.filter((c) => c.patientId === p.id);
+          return [
+            ...a,
+            ...consults.map((c) => ({
+              id: c?.spectacle?.id,
+              recallDate: c?.recallDate,
+              recallDescription: c?.recallDescription,
+              firstName: p.firstName,
+              lastName: p.lastName,
+              school: p.school,
+              patientId: c?.patientId,
+            })),
+          ];
+        }, [])
+        .filter((r) =>
+          debouncedDateQuery
+            ? dayjs(r.recallDate).isBefore(debouncedDateQuery)
+            : true
+        )
+        .filter((r, i, a) => a.findIndex((t) => t.id === r.id) === i && r.id);
+      setRecallRecords(records);
+      setTableRecords(sortBy(records, 'recallDate'));
+    }
+  }, [matchingPatients, matchingConsults, debouncedDateQuery]);
 
   return (
     <ScrollArea className="h-full p-8">
       <Stack className={'w-4/5 mx-auto min-h-fit'}>
         <Title order={3}>Recall Records</Title>
         <TextInput
-          className="print:hidden"
           placeholder="Search dispensing records..."
           icon={<IconSearch size={16} />}
           value={query}
           onChange={(e) => setQuery(e.currentTarget.value)}
+        />
+        <DatePicker
+          allowFreeInput
+          className="w-36"
+          inputFormat="DD/MM/YYYY"
+          dateParser={(date: string) =>
+            dayjs(date, ['DD/MM/YYYY', 'DD/MM/YY']).toDate()
+          }
+          placeholder="Find all prior to date..."
+          initialMonth={new Date()}
+          value={dateQuery}
+          onChange={setDateQuery}
         />
         <DataTable
           sx={
@@ -75,23 +239,33 @@ export const RecallsListPage = () => {
           striped
           highlightOnHover
           className="min-h-full"
+          classNames={{ header: `${styles['datatable-custom-header']}` }}
           records={tableRecords}
-          idAccessor="uid"
+          idAccessor="id"
           columns={[
             {
               accessor: 'recallDate',
               title: 'DATE',
-              render: ({ recallDate }) =>
-                dayjs(recallDate).format('DD/MM/YYYY'),
+              render: ({ recallDate }) => {
+                return dayjs(recallDate).format('DD/MM/YYYY');
+              },
               sortable: true,
             },
-            { accessor: 'recallDescription', title: 'REASON', sortable: true },
+            {
+              accessor: 'recallDescription',
+              title: 'DESCRIPTION',
+              sortable: true,
+            },
+            { accessor: 'firstName', title: 'FIRST NAME', sortable: true },
+            { accessor: 'lastName', title: 'LAST NAME', sortable: true },
+            { accessor: 'school', title: 'SCHOOL', sortable: true },
           ]}
           sortStatus={sortStatus}
           onSortStatusChange={setSortStatus}
           noRecordsText="No records to show"
-          fetching={isFetching}
-          // onRowClick={(record) => navigate(`/spectacles-details/${record.uid}`)}
+          onRowClick={(record) =>
+            navigate(`/patient-details?patientId=${record.patientId}`)
+          }
         />
       </Stack>
     </ScrollArea>
